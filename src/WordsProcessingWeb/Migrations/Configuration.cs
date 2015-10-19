915b0639a7c1717722d91c9b92bc5a62620f779e
Denis Babarykin <babarykin.denis@gmail.com>
2015-10-19 22:34:51 +0300

    Database seeded.

--- src/WordsProcessingWeb/Migrations/Configuration.cs
+++ src/WordsProcessingWeb/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace WordsProcessingWeb.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.IO;
@@ -22,24 +23,27 @@ namespace WordsProcessingWeb.Migrations
             context.Dictionaries.Add(UKDictionary);
             context.SaveChanges();
 
-            StreamReader rusStream = new StreamReader(@"C:\Users\valeriya\Desktop\testing\t06-lab01\src\Lab1\bin\Debug\Dictionaries\pro-lingRus.txt");
+            string DataPath = AppDomain.CurrentDomain.BaseDirectory + "/../App_Data/TxtDictionaries";
+
+            StreamReader rusStream = new StreamReader(DataPath + "/pro-lingRus.txt");
             string line;
+            List<Word> words = new List<Word>();
             while ((line = rusStream.ReadLine()) != null)
             {
-                Word word = new Word { Text = line, Dictionary = rusDictionary };
-                context.Words.Add(word);                
+                words.Add(new Word { Text = line, Dictionary = rusDictionary });
             }
             rusStream.Close();
+            context.Words.AddRange(words);
             context.SaveChanges();
 
-            StreamReader UKStream = new StreamReader(@"C:\Users\valeriya\Desktop\testing\t06-lab01\src\Lab1\bin\Debug\Dictionaries\UK.txt");
-            
+            StreamReader UKStream = new StreamReader(DataPath + "/UK.txt");
+            words.Clear();
             while ((line = UKStream.ReadLine()) != null)
             {
-                Word word = new Word { Text = line, Dictionary = UKDictionary };
-                context.Words.Add(word);                
+                words.Add(new Word { Text = line, Dictionary = UKDictionary });
             }
             UKStream.Close();
+            context.Words.AddRange(words);
             context.SaveChanges();         
         }
     }
