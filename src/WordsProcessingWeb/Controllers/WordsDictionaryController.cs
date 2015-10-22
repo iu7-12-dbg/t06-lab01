37147bc18fdfafab7fc5c210c5682fe94d99393d
unknown <valeriya.syomina@yandex.ru>
2015-10-22 13:26:33 +0400

    ff

--- src/WordsProcessingWeb/Controllers/WordsDictionaryController.cs
+++ src/WordsProcessingWeb/Controllers/WordsDictionaryController.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WordsProcessing;
+using WordsProcessing.Algorithms;
 using WordsProcessingWeb.DAL;
 using WordsProcessingWeb.Models;
 
@@ -42,14 +44,37 @@ namespace WordsProcessingWeb.Controllers
             
          //   db.SaveChanges();
 
-            return View(db.Dictionaries.ToList());
+            List<Word> words = new List<Word>();
+            return View(words);
         }
 
         [HttpPost]
-        public void CheckText(string inputedText)
+        public ActionResult ResultWords(string inputedWord)
         {
-            
+            List<Word> words = new List<Word>();
+            if (inputedWord != null)
+            {
+                WordsDictionary dictionary = new WordsDictionary(new WagnerFischer());
+                string language = Request.Form["Language"].ToString();
+                if (language == "RUS")
+                {
+                    dictionary.Words = db.Words.Where(word => word.Dictionary.Name == "Rus").Select(word => word.Text).ToList();
+                }
+                else if (language == "ENG")
+                {
+                    dictionary.Words = db.Words.Where(word => word.Dictionary.Name == "UK").Select(word => word.Text).ToList();
+                }
+                List<string> closestWords = dictionary.GetClosestWords(inputedWord);
 
+                for (int i = 0; i < closestWords.Count; i++)
+                {
+                    Word word = new Word() { Text = closestWords[i] };
+                    words.Add(word);
+                }
+            }
+
+            
+            return View(words);
         }
 
     }
